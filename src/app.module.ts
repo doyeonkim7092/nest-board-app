f6dfecc7bb231424cced0dd4dd66f084277b56ad
doyeonKIM <gothone7092@gmail.com>
2022-09-25 12:31:56 +0900

    [master]ADD:boardServie

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsController } from './boards/boards.controller';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BoardsController],
-  providers: [AppService],
+  providers: [AppService, BoardsService],
 })
 export class AppModule {}
