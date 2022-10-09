8c32e08d2c71fe72b608d3875bb78ab9ee3fcf2b
doyeonKIM <gothone7092@gmail.com>
2022-10-09 21:25:43 +0900

    [master]ADD:boardModdel

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,8 +10,8 @@ export class BoardsController {
   //   }
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoard() {
+  @Get('/')
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
   }
 }
